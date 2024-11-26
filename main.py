d43c597e2f5a331a6fc1cf7b20d5beffd59f0ee6
Nonyerem23 <mc.chinonye@stu.unizik.edu.ng>
2024-11-26 20:20:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("SECURE LOGIN")
+print("MY LOGIN SYSTEM")
+print("++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "David" and password == "totallyNotBaldy":
+  print("Why hello there David, what a lovely accent you have, you could have charmed your way in here even without a password.")
+  print("Have a great day.")
+  print("Don't forget to wear a hat in the sun!")
+elif username == "Nonye" and password == "Nonye74nne":
+  print("Hey there Nonye!")
+else:
+  print("Go away!")
+  
